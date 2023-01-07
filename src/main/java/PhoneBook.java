9cfa2ab03849d0abd6ac668f8b39db07fb6813c8
Eugene Borisov <eugeni.borisov@gmail.com>
2023-01-07 14:30:49 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> contacts = new HashMap<>();
+
+    public int add(String name, String number) {
+        return 0;
+    }
+
 }
+//add - добавить имя с номером. Метод должен возвращать количество контактов после добавления, при этом гарантируется, что не будут добавляться повторяющиеся имена;
+//findByNumber - найти имя по номеру без полного перебора;
+//findByName - найти номер по имени без полного перебора;
+//printAllNames - вывести все имена в алфавитном порядке без необходимости проводить сортировку.
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,29 @@
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class PhoneBookTest {
+
+    private static PhoneBook phoneBook = new PhoneBook();
+
+    @BeforeAll
+    public static void startTesting() {
+        System.out.println("Start testing class phoneBook");
+    }
+
+    @AfterAll
+    public static void finishedTesting() {
+        System.out.println("Testing class phoneBook is complete");
+    }
+
+    @Test
+    public void testAddContact() {
+        int result = phoneBook.add("A", "1");
+        int expected = 1;
+        Assertions.assertEquals(expected, result);
+    }
 }
